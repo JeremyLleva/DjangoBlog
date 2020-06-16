40c6d3945e3ae19b54f5f6e7978e4578a2acf841
JeremyLleva <llevajeremy@yahoo.com>
2020-06-16 14:10:50 -0700

    Added user login and logout features

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 # Create your views here.
@@ -8,13 +9,14 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            # Stores name in python typescript and send flash message to say account created 
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created! You are now able to log in')
+            return redirect('login')
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form':form})
+    return render(request, 'users/register.html', {'form': form})
 
-    #Test changes
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
